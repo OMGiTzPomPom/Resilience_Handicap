b286f5fe886c918952b8d40528f2d7b9fea8cb09
extremety1989 <andreazure1989@hotmail.fr>
2023-03-24 06:43:49 +0100

    starting point

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LPIOTIA2023_Resilience_Handicap
+
+docker compose -f ./db/docker-compose.yml up -d && docker compose -f ./caddy/docker-compose.yml up -d && docker compose -f ./backend/docker-compose.yml up -d
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-    res.send('hello world')
-  })
+
+app.use(express.json());
+ 
+app.post('/register', function (req, res) {
+    console.log(req.body.name)
+    res.end();
+})
+
+app.post('/login', function (req, res) {
+  console.log(req.body.name)
+  res.end();
+})
+
+app.get('/get-parking', function (req, res) {
+  console.log(req.query.area)
+  res.end();
+})
   
 app.listen(3000)
