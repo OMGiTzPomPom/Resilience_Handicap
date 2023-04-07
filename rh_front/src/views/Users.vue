2bf386ad7cf7b08185fa29e50b486ec95f4d5ae9
OMGiTzPomPom <pauline.srifi@gmail.com>
2023-04-07 12:02:47 +0200

    user vue is displayed

--- rh_front/src/views/Users.vue
+++ rh_front/src/views/Users.vue
@@ -1,55 +1,71 @@
 <script setup>
 
 import {onMounted, reactive} from "vue";
+import dayjs from "dayjs";
 
 let users = reactive([]);
 
 onMounted(async () => {
-            const settings = {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                }
-            }
-        try {
-            const fetchResponse = await fetch("http://localhost:3300/users?page=1", settings);
-            const data = await fetchResponse.json();
-            
-            console.log(data)
-            users = data.users
-            console.log(users)
-            } catch (error) {
-            console.log(error);
+    const settings = {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
         }
+    }
+    try {
+        const fetchResponse = await fetch("http://localhost:3300/users?page=1", settings);
+        const data = await fetchResponse.json();
+
+        console.log(data)
+
+        data.users.forEach(el => {
+            users.push(el)
+        })
+
+        //users = data.users
+        //console.log(users)
+    } catch (error) {
+        console.log(error);
+    }
 });
 </script>
 
-<template class="text-center">
-    <table class="table table-striped">
-          <thead>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First name</th>
-                    <th scope="col">Last name</th>
-                    <th scope="col">Is disabled ?</th>
-                    <th scope="col">Until </th>
-                    <th scope="col" colspan="2">Actions </th>
-                    </tr>
-                </thead>
-                <tbody v-for="user in users" :key="user.id">
-                    <tr>
-                    <th scope="row">{{ user.id }}</th>
-                    <td>{{ user.first_name }}</td>
-                    <td>{{ user.last_name }}</td>
-                    <td>{{ user.is_disabled }}</td>
-                    <td>{{ user.until }}</td>
-                    <td><button type="button" class="btn btn-warning">Modify</button><button type="button" class="btn btn-danger">Delete</button></td>
-                    </tr>
-                </tbody>
-    </table>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col mx-auto">
+                <table class="table table-responsive table-striped mx-auto">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="">First name</th>
+                            <th scope="col" class="">Last name</th>
+                            <th scope="col" class="">Is disabled ?</th>
+                            <th scope="col" class="">Until </th>
+                            <th scope="col" class="" colspan="2">Actions </th>
+                         </tr>
+                    </thead>
+                    <tbody v-for="user in users" :key="user.id">
+                        <tr>
+                            <td>{{ user.first_name }}</td>
+                            <td>{{ user.last_name }}</td>
+                            <td v-if="user.is_disabled == 1">Yes</td>
+                            <td v-else>No</td>
+                            <td>{{ dayjs(user.until).format('DD/MM/YYYY') }}</td>
+                            <td>
+                                <button type="button" class="btn btn-warning btn-sm">Modify</button>
+                                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-
+button {
+    margin: 0px 5px;
+}
 </style>
